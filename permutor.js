496184330aeb568a718fe6a45afa075509294288
byronicone <byron.e.barnes@gmail.com>
2018-05-09 11:00:51 -0500

    Code is functional, but performance needs improvement.  Any number larger than 4 cubes takes an inordinate amount of time.  Perhaps look into some async solutions or mathematical properties/assumptions

--- permutor.js
+++ permutor.js
@@ -1,10 +1,11 @@
 var exports = module.exports = {};
 
-exports.getPermutations = function permute (prefix, str, resultSet){
+exports.getCubedPermutations = function permute (prefix, str, resultSet){
   let len = str.length;
   if(len==0){
-    if(prefix.charAt(0)!=0){
-      resultSet.add(prefix);
+    let permutation = parseInt(prefix);
+    if(prefix.charAt(0)!=0 && Math.cbrt(permutation) % 1 ==0){
+      resultSet.add(permutation);
     }
   }
   for(let i=0; i< len; i++){
@@ -14,30 +15,40 @@ exports.getPermutations = function permute (prefix, str, resultSet){
   }
 }
 
+
+// exports.getPermutations = function permute (prefix, str, resultSet){
+//   let len = str.length;
+//   if(len==0){
+//     if(prefix.charAt(0)!=0){
+//       resultSet.add(prefix);
+//     }
+//   }
+//   for(let i=0; i< len; i++){
+//     let newPrefix = prefix+str.charAt(i);
+//     let newStr = str.substr(0,i) + str.substr(i+1);
+//     permute(newPrefix, newStr, resultSet);
+//   }
+// }
+
 exports.findOtherCubes = (num) => {
   //test return [56623104,66430125];
   //1. cube the number
   let cubed = Math.pow(num, 3);
   let set = new Set();
   //2. get all permutations of number
-  module.exports.getPermutations('', cubed.toString(), set);
-
-  //3. get the cubed root of each number
-  //4. if it is not fractional, add to result set
-  let result = [...set].filter( perm => (Math.cbrt(perm) % 1 ==0)
-    && perm!=cubed ).map( permutation => parseInt(permutation));
+  module.exports.getCubedPermutations('', cubed.toString(), set);
 
   //5. return the list of numbers with cubed roots
-  return result;
+  return set;
 }
 
 exports.getSmallestCube = (howMany) => {
   let looking = true;
-  let number = 1;
+  let number = 2000;
   do{
     console.log(number);
     let cubes = exports.findOtherCubes(number)
-    if (cubes.length == howMany){
+    if (cubes.size == howMany){
       console.log(cubes);
       return Math.pow(number, 3);
     }
--- test/cubic-test.js
+++ test/cubic-test.js
@@ -8,7 +8,7 @@ var exampleString = 'ABC';
 describe('Permutor', () => {
   it('finds other permutations of '+exampleInt+' that are cubes', () => {
     let cubes = permutor.findOtherCubes(exampleInt);
-    assert.deepEqual(cubes, [66430125,56623104]);
+    assert.deepEqual(cubes, [4106362,566430125,56623104]);
   })
   it('finds all permutations of string ' + exampleString, () => {
     let set = new Set();
@@ -18,5 +18,5 @@ describe('Permutor', () => {
   it('finds the smallest cubed number (41063625) that has 3 permutations that are also cubes', () => {
     let smallestCube = permutor.getSmallestCube(2);
     assert.equal(smallestCube, 41063625);
-  }).timeout(10000);
+  })
 });
